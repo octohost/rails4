e7e14e53286d72d983050c72328238960b65b15f
Darron Froese <darron@froese.org>
2013-12-09 19:40:48 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+Rails 4.0.x
+====
+
+Basic [Rails 4.0.x](http://rubyonrails.org/) container for octohost.
+
+Clone this repo and use the Ruby on Rails to develop your website.
+
+Push this repo to your octohost:
+
+```
+git clone https://github.com/octohost/rails4.git
+cd harp
+git remote add octohost git@ip.address.here:rails4.git
+git push octohost master
+```
+
+Example site \(usually\) at [http://rails4.octohost.io](http://rails4.octohost.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4::Application.config.secret_key_base = '35e29fd02872174caf6e6e2a797d1a1aac8a9211593919e547ce0b62bbb4ee3a961970d3865836d0e0df1a21d5b714169e1b8742c5c2cb20c03ead3b98b784b9'
